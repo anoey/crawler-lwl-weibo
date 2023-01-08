04b066ed57478d8e9e8f340da974e7987a900823
anoey <ewpiqt05@duck.com>
2023-01-08 11:56:55 +0800

    fix filter

--- main.ts
+++ main.ts
@@ -1,36 +1,46 @@
-import { Comment, commentsCollection} from "./mongo.ts"
+import { Comment, commentsCollection } from "./mongo.ts";
 
 async function getComments() {
-  const targetUrl = 'https://weibo.com/ajax/statuses/buildComments?is_reload=1&id=4467107636950632&is_show_bulletin=2&count=10'
+  const targetUrl =
+    "https://weibo.com/ajax/statuses/buildComments?is_reload=1&id=4467107636950632&is_show_bulletin=2&count=10";
 
-  const res = await fetch(targetUrl)
-  const data = await res.json()
+  const res = await fetch(targetUrl);
+  const data = await res.json();
 
   // deno-lint-ignore no-explicit-any
   const comments = data.data.map((item: any) => ({
     content: item.text_raw,
     user: item.user.screen_name,
     time: new Date(item.created_at).getTime(),
-  }))
+  }));
 
-  return comments
+  return comments;
 }
-let oldComments = (await commentsCollection.find().toArray()).splice(0, 10)
+
+let oldComments = (await commentsCollection.find().toArray())
+  .sort(
+    (a, b) => new Date(b.time).getTime() - new Date(a.time).getTime(),
+  )
+  .slice(0, 20);
 
 while (true) {
-  const comments = await getComments()
+  const comments = await getComments();
 
   // filter out the old comments
   const insertComments = comments.filter((comment: Comment) => {
-    return !oldComments.some((oldComment: Comment) => oldComment.content === comment.content)
-  })
+    return !oldComments.some(
+      (oldComment: Comment) =>
+        oldComment.content === comment.content &&
+        oldComment.user === comment.user,
+    );
+  });
 
   if (insertComments.length !== 0) {
-    commentsCollection.insertMany(insertComments)
+    commentsCollection.insertMany(insertComments);
   }
-  oldComments = comments
+  oldComments = comments;
 
-  console.log('inserted', insertComments.length, 'comments')
+  console.log("inserted", insertComments.length, "comments");
 
-  await new Promise((resolve) => setTimeout(resolve, 1000 * 60))
+  await new Promise((resolve) => setTimeout(resolve, 1000 * 60));
 }
